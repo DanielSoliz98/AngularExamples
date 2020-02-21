f767a8d0e5373a2b1c8b2878edf7bde276103cf2
Daniel Soliz <danielsoliz98@hotmail.com>
2020-02-20 21:41:23 -0400

    form group example added

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.css"]
 })
 export class FormComponent implements OnInit {
-
-  constructor() { }
+  exampleForm = new FormGroup({
+    movie: new FormControl({ value: "", disabled: true }, Validators.required)
+  });
+  constructor() {}
 
   ngOnInit(): void {
+    this.exampleForm.get("movie").valueChanges.subscribe(value => {
+      this.imprimir(value);
+    });
   }
 
+  get movie() {
+    return this.exampleForm.get("movie");
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with   value
+    console.log(this.movie.value);
+  }
+
+  enable() {
+    this.exampleForm.get("movie").enable();
+  }
+  private imprimir(value: string) {
+    console.log(value);
+  }
 }
